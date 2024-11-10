e8ff2ad01e8d283c78ee578191c0d35fd6bfedfc
FathirMaulanaP <fathirmaulanaputra83@gmail.com>
2024-11-10 08:39:57 +0700

    Add files via upload

---
+++ Loopinig.java
@@ -0,0 +1,15 @@
+
+public class Loopinig {
+    public static void main(String[] args) {
+        for(int x=1;x <= 4; x++ ){
+            for(int y=1;y <= 4; y++){
+                if(y <= 4 - x){
+                    System.out.print("s ");
+                }else{
+                    System.out.print("o ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
